53c74301ccc6c2f8d0a0b56176072e1e37b6325e
Hardik Kalsule <hardikslaptop0710@gmail.com>
2025-06-12 15:55:03 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class prac2 {
+    public static void main(String[] args) {
+        // Arithematic Operators
+        int a = 20;
+        int b = 10;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // Assignment Operators
+        int c = 30;
+        c += 30;
+        System.out.println(c);
+
+        // Comaparison Operators
+        System.out.println(a == b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+
+        // Logical Operators
+        int d = 5;
+        System.out.println(d>1 && d<6);
+        System.out.println(d>3 || d<1);
+
+        // Bitwise Operators
+        System.out.println(a & b);
+        System.out.println(a | b);
+    }}
+
