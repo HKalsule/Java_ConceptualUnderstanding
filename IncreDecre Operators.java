1bb29a5b249aac6e4cc39d76b5e57b9d078b9e8e
Hardik Kalsule <hardikslaptop0710@gmail.com>
2025-06-12 16:25:20 +0530

    Add files via upload

---
+++ IncreDecre Operators.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class IncreDecre{
+    public static void main(String[] args){
+
+        // Increment Operators
+        int a = 10;
+        System.out.println(a++); // Increment after 
+        System.out.println(a);
+        System.out.println(++a); // Increment Befor
+        System.out.println(a);
+
+        // Decrement Operators
+        int b = 10;
+        System.out.println(b--); // Decrement after 
+        System.out.println(b);
+        System.out.println(--b); // Decrement Befor
+        System.out.println(b);
+    }
+}
