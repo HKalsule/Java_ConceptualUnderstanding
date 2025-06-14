dc7413e5b4c10fe277568d0c06ddb439ae882c27
Hardik Kalsule <hardikslaptop0710@gmail.com>
2025-06-14 11:59:24 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,24 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("For Logical AND...");
+        if(a && b){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+        
+        System.out.println("For Logical OR...");
+        if(a || b){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
+    
+
